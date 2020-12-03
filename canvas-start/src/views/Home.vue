0692f1f31eea6c8b8a75e8a85a335a43e4768570
BJ-HUANG <420749518@qq.com>
2020-12-03 16:37:20 +0800

    first commit

---
+++ canvas-start/src/views/Home.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="home">
+    <canvas id="canvas" width="150" height="150"></canvas>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "Home",
+});
+</script>
